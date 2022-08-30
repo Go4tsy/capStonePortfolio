04d7704639a742a56e1d76f36242213fbc49c9cd
go4tsy <mosesetienne@hotmail.com>
2022-08-30 10:00:26 -0400

    banner and tailwindcss download

--- goatsy/src/App.js
+++ goatsy/src/App.js
@@ -1,23 +1,67 @@
 import './App.css';
-import { Routes, Route, Switch, useLocation } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import { Route, Switch, useLocation } from 'react-router-dom'
+
+
 import Home from './Components/Home.js'
 import Footer from './Footer.js';
 import Header from './Header';
+
 import Contact from './Components/Contact';
 import Project from './Project Components/Project';
+import Banner from './Banner/Banner';
 
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    menuOpen === true ? setMenuOpen(false) : setMenuOpen(true)
+  }
+
+  let fullPath = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [fullPath])
+
+  useEffect(() => {
+    if (menuOpen) {
+      document.body.style.overflow = 'hidden';
+      return () => document.body.style.overflow = 'unset';
+    }
+  }, [menuOpen])
+
+
+
   return (
-    <>
-      <Header />
-      <Routes>
-        <Route path='/bio' element={<Home />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/projects' element={<Project />} />
-      </Routes>
-      <Footer />
-    </>
+    <div className='app'>
+      <Switch>
+        <Route path='/' exact>
+          <Banner menuOpen={menuOpen} toggleMenu={toggleMenu} />
+        </Route>
+
+
+        <Route path='/bio' exact >
+          <Header title="bio" menuOpen={menuOpen} toggleMenu={toggleMenu} />
+          <Home />
+          <Footer />
+        </Route >
+
+        <Route path='/contact' exact>
+          <Header title="contact" menuOpen={menuOpen} toggleMenu={toggleMenu} />
+          <Contact />
+          <Footer />
+        </Route >
+
+        <Route path='/projects' exact>
+          <Header title="projects" menuOpen={menuOpen} toggleMenu={toggleMenu} />
+          <Project />
+          <Footer />
+        </Route>
+      </Switch>
+    </div>
+
   );
 }
 
---
+++ goatsy/src/Banner/Banner.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useEffect } from 'react';
+
+import backgroundImage from '../Photos/bluematrix.png';
+import logo from '../Photos/logo.jpg';
+
+import HamburgerMenu from 'react-hamburger-menu';
+
+import './Banner.css';
+import Menu from '../Menu/Menu';
+
+function Banner({ menuOpen, toggleMenu }) {
+
+   
+    useEffect(() => {
+        window.addEventListener("scroll", toggleMenu);
+        return () => {
+            window.removeEventListener("scroll", toggleMenu);
+        }
+    }, [])
+
+    return (
+        <div className="banner-container" onScroll={toggleMenu}>
+            {!menuOpen && 
+                <div className="banner-logo-container">
+                    <img className="banner-logo" src={logo} alt="Goatsy" onClick={toggleMenu} />
+                </div>
+            }
+            <div className="banner-background-container">
+                <img className="banner-background-image" src={backgroundImage} alt="Matrix background" />
+            </div>
+            <div className="banner-hamburger-menu">
+                <HamburgerMenu 
+                        className="hamburger-menu" 
+                        isOpen={menuOpen} 
+                        menuClicked={toggleMenu} 
+                        color={"white"} 
+                        width={21}
+                        height={14}    
+                    />
+            </div>
+            { menuOpen && <Menu toggleMenu={toggleMenu} />}
+        </div>
+    )
+}
+
+export default Banner;
